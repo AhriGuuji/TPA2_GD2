992aae8ad1500fef3c1799f1598d98ebe7b20d27
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-06-03 00:39:41 +0100

    Levels made

--- Assets/Scripts/BoxFuit.cs
+++ Assets/Scripts/BoxFuit.cs
@@ -3,7 +3,22 @@ using UnityEngine;
 public class BoxFuit : MonoBehaviour
 {
     [SerializeField] private string _fruitName;
+    [SerializeField] private int _maxStorage = 10;
+    [SerializeField] private Sprite _sprite;
     private Fruit _fruit;
+    private int _count;
+
+    void Start()
+    {
+        _count = 0;
+    }
+
+    void Update()
+    {
+        if (_count >= _maxStorage)
+            gameObject.GetComponent<SpriteRenderer>().sprite = _sprite;
+    }
+
     void OnCollisionStay2D(Collision2D collision)
     {
         _fruit = collision.gameObject.GetComponent<Fruit>();
@@ -11,8 +26,11 @@ public class BoxFuit : MonoBehaviour
         if (_fruit)
         {
             Destroy(collision.gameObject);
-            if(_fruit.name.Contains(_fruitName))
+            if (_fruit.name.Contains(_fruitName) && _count < _maxStorage)
+            {
+                _count++;
                 ScoreManager.Instance.AddScore(_fruit);
+            }
         }
     }
 }
--- Assets/Scripts/PlayerPickAndDrop.cs
+++ Assets/Scripts/PlayerPickAndDrop.cs
@@ -5,16 +5,12 @@ public class PlayerPickAndDrop : MonoBehaviour
 {
     [SerializeField] private int _gridSize;
     [SerializeField] private LayerMask _fruitLayer;
-    [SerializeField] private float _throwForce = 32f;
     private InputAction _interact;
-    private InputAction _boost;
     private RaycastHit2D _hit;
-    private RaycastHit2D _hit2;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         _interact = InputSystem.actions.FindAction("Interact");
-        _boost = InputSystem.actions.FindAction("Throw");
     }
 
     // Update is called once per frame
@@ -39,39 +35,6 @@ public class PlayerPickAndDrop : MonoBehaviour
             {
                 _hit.transform.GetComponent<Collider2D>().isTrigger = false;
                 _hit.transform.parent = null;
-                _hit = default;
-            }
-        }
-
-        if (PlayerAbilityManager.Instance.CanPickUp)
-        {
-            if (_boost.WasPressedThisFrame())
-            {
-                if (!_hit2)
-                {
-                    _hit2 = Physics2D.Raycast(transform.position, transform.up, distance: _gridSize, layerMask: _fruitLayer);
-
-                    if (_hit2)
-                    {
-                        _hit2.transform.SetParent(gameObject.transform);
-                        _hit2.transform.GetComponent<Collider2D>().isTrigger = true;
-                    }
-                }
-                else
-                {
-                    _hit2.transform.GetComponent<Collider2D>().isTrigger = false;
-                    _hit2.transform.parent = null;
-                    _hit2 = default;
-                }
-            }
-        }
-        
-        if (PlayerAbilityManager.Instance.CanThrow)
-        {
-            if (_boost.WasPressedThisFrame() && _hit)
-            {
-                _hit.transform.parent = null;
-                _hit.transform.GetComponent<Rigidbody2D>().AddForce(transform.up * _throwForce);
                 _hit = default;
             }
         }
